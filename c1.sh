27d57d90b4ec4e78ae7dab665bbf974daff4d881
yugecin <yugecin@users.noreply.github.com>
2018-08-31 11:04:17 +0200

    idea config files (and unquote paths)

--- c1.sh
+++ c1.sh
@@ -1,9 +1,16 @@
-cp "~/_ideavimrc" ".ideavimrc"
-cp "c:/bbZero/plugins.rc" "bbzero/plugins.rc"
-cp "c:/bbZero/menu.rc" "bbzero/menu.rc"
-cp "c:/bbZero/blackbox.rc" "bbzero/blackbox.rc"
-cp "c:/bbZero/plugins/BlackboxBar/BlackboxBar.rc" "bbzero/BlackboxBar.rc"
-cp "c:/bbZero/plugins/bbLeanSkin/bbLeanSkin.rc" "bbzero/bbLeanSkin.rc"
-cp "c:/bbZero/plugins/bbLeanSkin/exclusions.rc" "bbzero/exclusions.rc"
-cp "c:/bbZero/plugins/bbLeanSkin/bbLeanSkinMenu.rc" "bbzero/bbLeanSkinMenu.rc"
-cp "../keyrate.bat" "keyrate.bat"
+cp ~/_ideavimrc .ideavimrc
+cp c:/bbZero/plugins.rc bbzero/plugins.rc
+cp c:/bbZero/menu.rc bbzero/menu.rc
+cp c:/bbZero/blackbox.rc bbzero/blackbox.rc
+cp c:/bbZero/plugins/BlackboxBar/BlackboxBar.rc bbzero/BlackboxBar.rc
+cp c:/bbZero/plugins/bbLeanSkin/bbLeanSkin.rc bbzero/bbLeanSkin.rc
+cp c:/bbZero/plugins/bbLeanSkin/exclusions.rc bbzero/exclusions.rc
+cp c:/bbZero/plugins/bbLeanSkin/bbLeanSkinMenu.rc bbzero/bbLeanSkinMenu.rc
+cp ../keyrate.bat keyrate.bat
+
+IDEA182=~/.IdeaIC2018.2/config
+
+cp $IDEA182/colors/hh.icls idea182/hh.icls
+cp $IDEA182/colors/light2bit.icls idea182/light2bit.icls
+cp $IDEA182/options/ide.general.xml idea182/ide.general.xml
+cp $IDEA182/options/editor.xml idea182/editor.xml
